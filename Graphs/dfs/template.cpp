23146ded66c3c14e6288fdfdfb0874f87134b6d4
Viswazgummadi <viswazgummadi@gmail.com>
2025-05-14 20:32:24 +0530

    doen

--- Graphs/dfs/template.cpp
+++ Graphs/dfs/template.cpp
@@ -2,9 +2,12 @@
 
 using namespace std;
 
+int m, n;
+// n nodes and m edges
 vector<vector<int>> g;
 
 vector<int> vis;
+
 
 int main()
 {
@@ -14,19 +17,34 @@ int main()
 	int t = 1;
 	while(t--)
 	{
-		solve();
-	}
-	algo();
+		cin >> m >> n ;
+		//assign each time based on your context
+		//base template includes
 
-
-
-	for(int i=0;i<vis.length();i++)
-	{
-		if(vis[i] != 0)
+		g.resize(n+1);
+		for(int i = 0 ; i < n ; i++)
 		{
-			cout << i << " ";
+			int a,b;
+			cin >> a >> b;
+			g[a].push_back(b);
+			g[b].push_back(a);
 		}
-		cout << endl;
+		vis.assign(n+1,0);
+
+
+		algo();
+
+
+        	for(int i=0;i<vis.length();i++)
+        	{
+                	if(vis[i] != 0)
+        	        {
+                        cout << i << " ";
+                	}
+        	        cout << endl;
+	        }
+
+
 	}
 
 
