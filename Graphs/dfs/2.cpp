96782a9898cb409ac92dbdd5c581c6cf05ccf24a
Viswazgummadi <viswazgummadi@gmail.com>
2025-05-15 14:24:14 +0530

    doen

---
+++ Graphs/dfs/2.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int m,n;
+
+// n nodes m edges
+
+
+vector<vector<int>> g;
+vector<vector<int>> components;
+vector<int> comp_size;
+vector<int> vis;
+int size_this = 0;
+
+// vis[node] = 1/0
+
+
+
+void dfs(int node, int comp)
+{
+	vis[node] = 1;
+	components[comp].push_back(node);
+	size_this ++;
+
+	for(auto v : g[node])
+	{
+		if(!vis[v])
+		{
+			dfs(v,comp);
+		}
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+
+	g.resize(n+1);
+	components.resize(n+1);
+
+	for(int i =0 ; i < m ; i++)
+	{
+		int a,b;
+		cin >> a >> b ;
+		g[a].push_back(b);
+		g[b].push_back(a);
+
+
+	}
+	vis.assign(n+1,0);
+	comp_size.assign(n+1,0);
+
+
+	int comp = 0;
+
+	for(int i = 1 ; i < n+1 ; i++)
+	{
+		if(!vis[i])
+		{
+			dfs(i,comp);
+			comp++;
+			comp_size[comp] = size_this;
+			size_this = 0;
+		}
+
+	}
+
+	cout << "no of components : " << comp << endl;
+
+	cout << "components :" << endl;
+
+
+    for(int i = 0; i < comp;i++)
+    {
+        for(auto k : components[i])
+        {
+            cout << k << ",";
+        }
+        cout << endl;
+    }
+// 	for(auto i : components)
+// 	{
+// 		for(auto k : i)
+// 		{
+// 			cout << k << ",";
+// 		}
+// 		cout << endl;
+// 	}
+// 	cout << endl;
+
+	cout << "size of the components : " << endl;
+
+	for(int i = 0 ; i < n+1 ; i++)
+	{
+		if(comp_size[i])
+		{
+			cout << i << " : " << comp_size[i] << endl;
+		}
+	}
+
+	return 0;
+}
